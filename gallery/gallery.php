11f7a18b4c58b77eebb9edac9bac364ed969eb3a
sandy-7798 <60814567+sandy-7798@users.noreply.github.com>
2020-02-08 23:32:58 +0545

    Add files via upload

---
+++ gallery/gallery.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>W O R L D O F I T S O W N</title>
+    <link rel="stylesheet" href="gallery.css">
+    
+        
+
+</head>
+<body>
+      <div class="slogan">
+          " WORLD OF ITS OWN !!! " 
+      </div>
+      <div class="logout">
+        <div class="logout-name">Lalit Cahudhary</div>
+          <div class="logout-photo"><i class="fas fa-user"></i>
+            <div class="fully-logout">Log Out</div>
+            </div>
+          
+      </div>
+    <div class="translate">
+        <div id="google_translate_element"></div>
+
+        <script type="text/javascript">
+            function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                    }
+        </script>
+
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    </div>
+
+
+    <div class="container">
+         <div class="box">
+                <div class="imgbox">
+                    <img src="lumbini.jpg">
+
+                </div>
+                <div class="check-price">
+                    <div class="price"><label class="containers">$199
+                        <input type="checkbox" checked="checked">
+                        <span class="checkmark"></span>
+                      </label></div>
+                </div>
+                
+                 <div class="details">
+                       <div class="content"></div>
+                       <h2 class="text">LUMBINI</h2>
+                          <p>Birth place of Gautam Buddha(mayadevi temple)<br>
+                            
+                        </p>
+                        <a href="#"><input type="button" value="Read More" class="read-more"></a>
+                 </div>
+        </div>
+
+        <div class="box">
+            <div class="imgbox">
+                <img src="sauraha.jpg">
+            </div>
+            <div class="check-price">
+                <div class="price"><label class="containers">$299
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                  </label></div>
+            </div>
+            
+             <div class="details">
+                   <div class="content"></div>
+                   <h2 class="text">CHITWAN NATIONAL PARK</h2>
+                      <p>  The first national park of Nepal.It is a preserved area in the Terai Lowlands of south-central Nepal, known for its biodiversity <br>
+                        <a href="#"><input type="button" value="Read More" class="read-more"></a></p>
+             </div>
+        </div>
+              
+        <div class="box">
+        <div class="imgbox">
+            <img src="tilicho.jpg">
+        </div>
+        <div class="check-price">
+            <div class="price"><label class="containers">$259
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label></div>
+        </div>
+         <div class="details">
+               <div class="content"></div>
+               <h2 class="text">TILICHO LAKE</h2>
+                  <p>Tilicho Lake is a lake located in the Manang district of Nepal, 55 kilometres (34 mi) as the crow flies from the city of Pokhara.
+                     It is situated at an altitude of 4,919. <br>
+                    <a href="#"><input type="button" value="Read More" class="read-more"></a>
+
+                  </p>
+         </div>
+        </div>
+
+        <div class="box">
+            <div class="imgbox">
+                <img src="pashupati.jpg">
+            </div>
+            <div class="check-price">
+                <div class="price"><label class="containers">$9
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                  </label></div>
+            </div>
+             <div class="details">
+                   <div class="content"></div>
+                   <h2 class="text">PASHUPATINATH TEMPLE</h2>
+                      <p>The Pashupatinath Temple  is a famous and sacred Hindu temple complex that is located on the banks of the Bagmati River.
+                        Dedicated to Lord Shiva, Pashupatinath is one of the four most important religious sites in Asia for devotees of Shiva. <br>
+                        <a href="#"><input type="button" value="Read More" class="read-more"></a></p>
+             </div>
+    </div>
+
+    <div class="box">
+        <div class="imgbox">
+            <img src="janaki.jpg">
+        </div>
+        <div class="check-price">
+            <div class="price"><label class="containers">$199
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label></div>
+        </div>
+         <div class="details">
+               <div class="content"></div>
+               <h2 class="text">JANAKI TEMPLE</h2>
+                  <p>Janaki Mandir (Nepali: जानकी मन्दिर) is a Hindu temple in Janakpur in the Mithila region of Nepal, dedicated to the Hindu goddess Sita.
+                       <br>
+                    <a href="#"><input type="button" value="Read More" class="read-more"></a>
+
+                  </p>
+         </div>
+    </div>
+          
+    <div class="box">
+    <div class="imgbox">
+        <img src="lakeside.jpg">
+    </div>
+    <div class="check-price">
+        <div class="price"><label class="containers">$199
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label></div>
+    </div>
+     <div class="details">
+           <div class="content"></div>
+           <h2 class="text">LAKESIDE POKHARA</h2>
+              <p>The Tharu people are an ethnic group indigenous to the Terai in southern Nepal and northern India.<br>
+                <a href="#"><input type="button" value="Read More" class="read-more"></a>
+
+              </p>
+     </div>
+    </div>
+
+    <div class="box">
+        <div class="imgbox">
+            <img src="trek.jpg">
+        </div>
+        <div class="check-price">
+            <div class="price"><label class="containers">$199
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+              </label></div>
+        </div>
+         <div class="details">
+               <div class="content"></div>
+               <h2 class="text">TREKKING</h2>
+                  <p> The Everest Base Camp Trek is the best option for those looking to
+                       relish the spectacular close up views of the world's highest mountains<br>
+                    <a href="#"><input type="button" value="Read More" class="read-more"></a>
+
+                  </p>
+         </div>
+</div>
+
+<div class="box">
+    <div class="imgbox">
+        <img src="bunjee.jpg">
+    </div>
+    <div class="check-price">
+        <div class="price"><label class="containers">$199
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+          </label></div>
+    </div>
+     <div class="details">
+           <div class="content"></div>
+           <h2 class="text">BUNJEE JUMPING</h2>
+              <p>The ultimate thrill of a Bungy jump can now be experienced in Nepal at perhaps the best site in the world. Nepal's first bungy jumping site
+                   is situated 160m over the wild Bhote Kosi river, and located close to the Nepal<br>
+                <a href="#"><input type="button" value="Read More" class="read-more"></a>
+
+              </p>
+     </div>
+</div>
+      
+<div class="box">
+<div class="imgbox">
+    <img src="sinja.jpg">
+</div>
+<div class="check-price">
+    <div class="price"><label class="containers">$199
+        <input type="checkbox" checked="checked">
+        <span class="checkmark"></span>
+      </label></div>
+</div>
+ <div class="details">
+       <div class="content"></div>
+       <h2 class="text">SINJA VALLEY</h2>
+          <p>The Sinja Valley is located in the Jumla District in Karnali Province.
+            The Sinja Valley was where the Nepali language originates from .<br>
+            <a href="#"><input type="button" value="Read More" class="read-more"></a>
+        </p>
+</div>
+</div>
+<div class="bottoms">
+    <a href="#" class="button"> Proceed <i class="fas fa-arrow-right"></i></a>
+</div>
+     
+    
+         
+
+</div>
+
+
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
